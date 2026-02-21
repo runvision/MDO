5913dcd4ddae0d08fb8f4ca61b432fd9aad7e177
RunVision <contatomariofarias@gmail.com>
2026-02-20 21:53:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -14,22 +14,20 @@ from "https://www.gstatic.com/firebasejs/12.9.0/firebase-database.js";
 // CONFIG FIREBASE
 
 const firebaseConfig = {
-
-apiKey: "AIzaSyCUNjxXY7-qq961qv0MudKgCcrMchVVzQQ",
-
-authDomain: "mdo-acro.firebaseapp.com",
-
-databaseURL: "https://mdo-acro-default-rtdb.firebaseio.com",
-
-projectId: "mdo-acro",
-
-storageBucket: "mdo-acro.firebasestorage.app",
-
-messagingSenderId: "957477543792",
-
-appId: "1:957477543792:web:8009e8d70c2cc4e295f9b5"
-
-};
+    apiKey: "AIzaSyCRwj51LdP7vYdf9NcQJJ4OmB6-gN90LRk",
+  
+    authDomain: "mdo-acro-8f0bc.firebaseapp.com",
+  
+    databaseURL: "https://mdo-acro-8f0bc-default-rtdb.firebaseio.com",
+  
+    projectId: "mdo-acro-8f0bc",
+  
+    storageBucket: "mdo-acro-8f0bc.firebasestorage.app",
+  
+    messagingSenderId: "112983273830",
+  
+    appId: "1:112983273830:web:16387d17530660ad9f58a7"
+  };
 
 
 // INICIAR FIREBASE
